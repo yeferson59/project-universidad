863643c32edc3027f68d94529221c3c3bb7f0d58
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-04-11 11:28:39 -0500

    fix: Import everything from base settings in local.py

--- template-jango/proyecto_django/proyecto_django/settings/local.py
+++ template-jango/proyecto_django/proyecto_django/settings/local.py
@@ -1,5 +1,4 @@
-# noqa: F403
-from .base import BASE_DIR
+from .base import *
 from decouple import config, Csv
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
