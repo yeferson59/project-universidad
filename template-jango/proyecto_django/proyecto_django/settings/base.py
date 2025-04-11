67ce4e52c6a3e69eaafed9490a09f31c8e7c7759
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-04-11 12:30:56 -0500

    feat: Add test_utils to LOCAL_APPS in Django settings

--- template-jango/proyecto_django/proyecto_django/settings/base.py
+++ template-jango/proyecto_django/proyecto_django/settings/base.py
@@ -22,15 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Application definition
 
 DJANGO_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
 )
 
-LOCAL_APPS = ()
+LOCAL_APPS = (
+  'applications.test_utils',
+)
 
 THIRD_PARTY_APPS = ()
 
