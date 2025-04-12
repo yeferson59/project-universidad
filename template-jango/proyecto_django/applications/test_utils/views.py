2598fd8926d35e258bbbbfeba2d7d9789318f8c3
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-04-11 19:25:45 -0500

    fix: Add docstring and comment out unused import in test_utils views

--- template-jango/proyecto_django/applications/test_utils/views.py
+++ template-jango/proyecto_django/applications/test_utils/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+"""Views for the test_utils application.
+
+This module contains the views for the test_utils application.
+"""
+
+# from django.shortcuts import render
 
 # Create your views here.
