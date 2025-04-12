ea0204d375734f49162ef913d47e903748dbf2b0
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-04-11 19:24:04 -0500

    fix:Replace admin imports with module docstring in test_utils

--- template-jango/proyecto_django/applications/test_utils/admin.py
+++ template-jango/proyecto_django/applications/test_utils/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+"""Module for test_utils.
+
+This module provides components for the test_utils.
+"""
+
+# from django.contrib import admin
 
 # Register your models here.
