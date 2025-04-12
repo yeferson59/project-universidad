62deb3f913ada2e0ac78d8ec3100ba2301d72f9a
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-04-11 19:26:44 -0500

    fix: Refactor local settings for consistency and documentation

--- template-jango/proyecto_django/proyecto_django/settings/local.py
+++ template-jango/proyecto_django/proyecto_django/settings/local.py
@@ -1,32 +1,37 @@
-from .base import *
-from decouple import config, Csv
+"""Local settings.
+
+Local settings for the Django project.
+"""
+
+from decouple import Csv, config
+
+from .base import BASE_DIR
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-  "default": {
-    "ENGINE": config('DB_ENGINE', default='django.db.backends.postgresql'),
-    "NAME": config('DB_NAME', default=''),
-    "USER": config('DB_USER', default=''),
-    "PASSWORD": config('DB_PASSWORD', default=''),
-    "HOST": config('DB_HOST', default=''),
-    "PORT": config('DB_PORT', default=''),
-  }
+    "default": {
+        "ENGINE": config("DB_ENGINE", default="django.db.backends.postgresql"),
+        "NAME": config("DB_NAME", default=""),
+        "USER": config("DB_USER", default=""),
+        "PASSWORD": config("DB_PASSWORD", default=""),
+        "HOST": config("DB_HOST", default=""),
+        "PORT": config("DB_PORT", default=""),
+    }
 }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
